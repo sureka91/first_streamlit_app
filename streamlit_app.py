338980190704d1fe9fd13fff2290648895d3a9a9
sureka91 <107801938+sureka91@users.noreply.github.com>
2022-06-19 20:10:38 +0530

    table below to list only selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,16 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 #though below helps. initially none selected
 #streamlit.multiselect("Pick some fruits : ", list(my_fruit_list.index)) 
+
 #pre-populated picklist
-streamlit.multiselect("Pick some fruits : ", list(my_fruit_list.index),['Avocado','Strawberries']) 
-streamlit.dataframe(my_fruit_list)
+#streamlit.multiselect("Pick some fruits : ", list(my_fruit_list.index),['Avocado','Strawberries']) 
+#streamlit.dataframe(my_fruit_list)
+
+#We'll ask our app to put the list of selected fruits into a variable called fruits_selected. 
+#Then, we'll ask our app to use the fruits in our fruits_selected list to pull rows from the full data set 
+#(and assign that data to a variable called fruits_to_show). Finally, we'll ask the app to use the data in 
+#fruits_to_show in the dataframe it displays on the page. 
+fruits_selected = streamlit.multiselect("Pick some fruits : ", list(my_fruit_list.index),['Avocado','Strawberries']) 
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+streamlit.dataframe(fruits_to_show)
