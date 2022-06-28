2f8e59799feb20156fdad16d46c39affce418f7f
sureka91 <107801938+sureka91@users.noreply.github.com>
2022-06-28 21:44:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,16 +38,22 @@ streamlit.dataframe(fruits_to_show)
 #streamlit.text(fruityvice_response)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-#import requests
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json()) #{'genus': 'Citrullus', 'name': 'Watermelon', 'id': 25, 'family': 'Cucurbitaceae', 'or
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_Choice:
+    streamlit.error("Please select a fruit to get information")
+  else: 
+    #streamlit.write('The user entered ', fruit_choice)
+    #import requests
+    #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    #streamlit.text(fruityvice_response.json()) #{'genus': 'Citrullus', 'name': 'Watermelon', 'id': 25, 'family': 'Cucurbitaceae', 'or
+    # write your own comment -what does the next line do? 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # write your own comment - what does this do?
+except URLError as e: 
+  streamlit.error()
+  
 streamlit.dataframe(fruityvice_normalized)
 streamlit.stop()
 
